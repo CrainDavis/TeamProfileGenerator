0c8efe2481d63fdc982ecbb6ab1960a776c3f204
Chyna Davis <chyna.davis11@gmail.com>
2020-04-15 00:54:58 -0700

    fix extra engineer/intern prompt bug

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // ===============================================================================
-
+// VARIABLES:
+// ===============================================================================
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -7,14 +8,20 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output") // directory path containing the generated HTML file
+const outputPath = path.join(OUTPUT_DIR, "team.html"); // file path for the generated HTML file
 
 const render = require("./lib/htmlRenderer");
 
-// ===============================================================================
+let employees = []; // array to hold the manager, engineer(s), and intern(s); will be passed into render() function
+let engineer = {}; // object to hold each created engineer
+let intern = {}; // object to hold each created intern
 
-function managerInfo() {
+// ===============================================================================
+// PROMPT FUNCTIONS FOR 3 EMPLOYEE ROLES (manager, engineer, intern):
+// ===============================================================================
+function getManagerInfo() {
     return inquirer.prompt([
         {
             type: "input",
@@ -54,7 +61,7 @@ function managerInfo() {
     ])
 }
 
-function engineerInfo() {
+function getEngineerInfo() {
     return inquirer.prompt([
         {
             type: "input",
@@ -94,7 +101,7 @@ function engineerInfo() {
     ])
 }
 
-function internInfo() {
+function getInternInfo() {
     return inquirer.prompt([
         {
             type: "input",
@@ -134,57 +141,71 @@ function internInfo() {
     ])
 }
 
-// ==============================================================================
-
-var employees = [];
-
-var engineer = {};
-var intern = {};
-
-async function pushEngineer() {
-    engineer = await engineerInfo();
+// ===============================================================================
+// FUNCTIONS:
+// ===============================================================================
+// create ENGINEER(s) based on user's answers to getEngineerInfo() prompt
+async function createEngineer() {
+    engineer = await getEngineerInfo();
     employees.push(new Engineer(engineer.name, engineer.id, engineer.email, engineer.github));
 }
 
-async function pushIntern() {
-    intern = await internInfo();
+// create INTERN(s) based on user's answers to getInternInfo() prompt
+async function createIntern() {
+    intern = await getInternInfo();
     employees.push(new Intern(intern.name, intern.id, intern.email, intern.school));
 }
 
-async function createHTML() {
+// function to compile all employee info and generate HTML page
+async function generateHtmlPage() {
     try {
-        manager = await managerInfo();
+        // create MANAGER based on user's answers to getManagerInfo() prompt
+        manager = await getManagerInfo();
         employees.push(new Manager(manager.name, manager.id, manager.email, manager.officeNumber));
 
-        var addMoreEmployees = true;
+        // create a situation (executed with while-loop) for when to allow user to add more engineers/interns
+        let addMoreEmployees = true;
         while(addMoreEmployees) {
+            // user adds manager, but no other employees
             if (manager.addMore === false) {
                 addMoreEmployees = false;
             }
+            // user also adds engineer(s)
             if (manager.newMember === "Engineer" || engineer.newMember === "Engineer" || intern.newMember === "Engineer") {
-                await pushEngineer();
+                intern.newMember = manager.newMember = engineer.newMember = "NONE";
+                await createEngineer();
+                // user stops adding employees
                 if (engineer.addMore === false) {
                     addMoreEmployees = false;
                 }
             }
+            // user also adds intern(s)
             if (manager.newMember === "Intern" || engineer.newMember === "Intern" || intern.newMember === "Intern") {
-                await pushIntern();
+                intern.newMember = manager.newMember = engineer.newMember = "NONE";
+                await createIntern();
+                // user stops adding employees
                 if (intern.addMore === false) {
                     addMoreEmployees = false;
                 }
             }
         }
-        console.log(employees);
 
+        console.log("generated employees:", employees);
+
+        // call the render function, passing in the employees array as an argument
         const renderedTeam = render(employees);
+        // write file ("team.html" will be generated inside the 'output' directory)
         await fs.writeFile(outputPath, renderedTeam, "utf8", (err) => {
             if (err) throw err;
-            console.log("Your team profile page has been successfully generated!");
+            console.log("SUCCESS! your team has been generated");
         });
         
     } catch (err) {
-        console.log(err);
+        console.log("ERROR!", err);
     }
 }
 
-createHTML();
+// ===============================================================================
+// CALL FUNCTION:
+// ===============================================================================
+generateHtmlPage();
